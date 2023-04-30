807d0e87a24e5cb72f7a938d86429e6884424e08
Sparsh Goyal <61899268+sparshgoyal2701@users.noreply.github.com>
2023-05-01 03:45:13 +0530

    Initial commit

---
+++ src/topscholars/UserID.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topscholars;
+
+/**
+ *
+ * @author K1LL3R
+ */
+public class UserID {
+    static int UserID;
+    static boolean flag = false;
+    public static String AccountType;
+    public static String Course;
+    /**
+     *
+     */
+    public static String [] temp = new String[11];
+    
+}
